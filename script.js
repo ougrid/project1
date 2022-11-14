35cb6bd3b36ca04e42a1cb64a72e8d3e2ccb7499
ougrid <ougridd@gmail.com>
2022-11-14 15:02:59 +0700

    add elements to index.html

--- script.js
+++ script.js
@@ -7,10 +7,23 @@ function addSequence() {
   console.log(sequence)
 }
 
+function showSequence() {
+  sequence.forEach(element => {
+    console.log(`show: ${element}`)
+    // $(`#${element}`).css("background-color", "secondary")
+    // $(`#${element}`).toggleClass("bg-secondary")
+    $(`#${element}`).text("THIS")
+    $(`#${element}`).addClass("opacity-75")
+    setTimeout(2000)
+    $(`#${element}`).removeClass("opacity-75")
+  })
+}
+
 $("#start").on("click", e => {   
   e.preventDefault()
+  sequence = []
   addSequence()
-  $(`${sequence[0]}`).css("backgroundColor", "white")
+  showSequence()
 })
 
 $("#green, #red, #yellow, #blue").on("click", e => {
