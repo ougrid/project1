0da3055cbfb87ad11bdd2a0c36a4ecafbda376a8
ougrid <ougridd@gmail.com>
2022-11-14 16:27:52 +0700

    add elements & initial random color sequence

--- script.js
+++ script.js
@@ -2,22 +2,53 @@ let colorBtns = ["green", "red", "yellow", "blue"]
 
 let sequence = []
 
+let playersSequence = []
+
 function addSequence() {
-  sequence.push(colorBtns[Math.floor(Math.random() * 4)])
+  sequence.push(colorBtns[Math.floor(Math.random() * colorBtns.length)])
   console.log(sequence)
 }
 
 function showSequence() {
-  sequence.forEach(element => {
+  
+  sequence.forEach((element) => {
     console.log(`show: ${element}`)
-    // $(`#${element}`).css("background-color", "secondary")
-    // $(`#${element}`).toggleClass("bg-secondary")
     $(`#${element}`).text("THIS")
-    $(`#${element}`).addClass("opacity-75")
-    setTimeout(2000)
-    $(`#${element}`).removeClass("opacity-75")
+    // setTimeout(element => $(`#${element}`).addClass("opacity-50"), 2000)
+    // $(`#${element}`).removeClass("opacity-50")
+    $(`#${element}`).addClass("opacity-50")
+    // TO DO: make it waits for a while then remove the class added!
   })
+
+  listenToPlayer()
 }
+
+function listenToPlayer() {
+  $("#green, #red, #yellow, #blue").on("click", e => {
+    e.preventDefault()
+    console.log(e.target)
+
+    if (playersSequence.length < sequence.length) {
+      playersSequence.push(e.target.id)
+      console.log(e.target.id);
+      console.log(`playersSequence: ${playersSequence}`)
+    } else if (playersSequence.length === sequence.length) {
+      let matchCheck = true
+      playersSequence.forEach((element, index) => {
+        if (element === sequence[index]) {
+          matchCheck = true
+        } else {
+          matchCheck = false
+          console.log('WRONG! Press "reset" to try again')
+          return 
+        }
+      })
+    } 
+    })
+}
+
+
+
 
 $("#start").on("click", e => {   
   e.preventDefault()
@@ -26,11 +57,3 @@ $("#start").on("click", e => {
   showSequence()
 })
 
-$("#green, #red, #yellow, #blue").on("click", e => {
-  e.preventDefault()
-  console.log(e.target)
-
-  if (colorBtns.includes(`${e.target.id}`) === true) {
-    console.log(e.target.id)
-  }
-})  
