5ab618bd436a001a2340e543c56cccfc527d5d13
ougrid <ougridd@gmail.com>
2022-11-15 11:28:37 +0700

    make fn: logMax

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ let sequence = []
 
 let playersSequence = []
 
+let setMaxScore = localStorage.setItem("maxScore", "0")
+
+let getMaxScore = localStorage.getItem("maxScore")
+
 function addSequence() {
   console.log("call addSequence()");
   sequence.push(colorBtns[Math.floor(Math.random() * colorBtns.length)])
-  console.log(sequence)
+  console.log(`added sequence: [${sequence}]`)
 }
-
-
 
 function showSequence() {
   console.log("call showSequence()");
@@ -23,14 +25,15 @@ function showSequence() {
     setTimeout(() => $(`#${element}`).removeClass("opacity-100"), (index + 1.5) * 500)
   })
 }
+
 // TO DO: make buttons blink when player clicks
 function listenToPlayer() {
   console.log("call listenToPlayer()");
   $("#green, #red, #yellow, #blue").on("click", e => {
     e.preventDefault()
-    console.log(`select: ${e.target.id}`)
+    console.log(`target: ${e.target.id}`)
     playersSequence.push(e.target.id)
-    console.log(`playersSequence: ${playersSequence}`)
+    console.log(`playersSequence: [${playersSequence}]`)
     if (playersSequence.length === sequence.length) {
       matchCheck()
     }
@@ -51,19 +54,43 @@ function matchCheck() {
   })
   console.log(`match: ${match}`)
   if (match === true) {
+    logMax(match)
     addSequence()
     showSequence()
     playersSequence = []
     console.log(`playersSequence: ${playersSequence}`)
   } else {
-    console.log("WRONG");
+    console.log("WRONG! Press 'Reset'");
     return
   }  
 }
 
+// let setMaxScore = localStorage.setItem("maxScore", "0")
+
+// let getMaxScore = localStorage.getItem("maxScore")
+
+function logMax (match) {
+  console.log("call logMax()")
+  console.log(`playersSequence.length: ${playersSequence.length}`)
+  // if (match === true) {
+    if (playersSequence.length > Number(getMaxScore)) {
+      console.log(`compare: \n  playersSequence.length: ${playersSequence.length}, getMaxScore: ${getMaxScore}`)
+      localStorage.removeItem("maxScore")
+      console.log(localStorage)
+      localStorage.setItem("maxScore", `${playersSequence.length}`)
+      console.log(localStorage)
+      console.log(`updated maxScore: ${getMaxScore}`)
+      // $("#score").text(`${getMaxScore}`)
+      $("#score").text(localStorage.getItem("maxScore"))
+    }
+  // }
+}
+
+// TO DO: log "RESET" when pressed
 function reset () {
   $("reset").on("click", e => {
     e.preventDefault
+    // console.log("RESET")
     gameInit()
   })
 }
@@ -81,6 +108,16 @@ function gameInit () {
 }
 
 gameInit()
+
+
+
+
+
+
+
+
+
+
 
 
 
